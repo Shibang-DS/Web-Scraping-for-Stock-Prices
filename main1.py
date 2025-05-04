82c95167cb2a6f8370b081b2531dc31c400af29f
Shibang <shibang.das.mec20@itbhu.ac.in>
2025-05-04 22:16:08 +0530

    demo commit

--- main1.py
+++ main1.py
@@ -1,6 +1,8 @@
 # Import the required libraries namely requests, BeautifulSoup ,pandas, openpyxl
 import requests
 from bs4 import BeautifulSoup
+import time
+import psutil
 import pandas as pd
 
 headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'}
